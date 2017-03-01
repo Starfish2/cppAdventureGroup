c98d480b62a0701b5a3ea76c2c28074d8107c40a
Cache Makers <Image15@iMac-D.local>
2017-02-28 17:56:46 -0700

    stuff that we did

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 #define WIDE 800
 #define HIGH 600
@@ -9,9 +10,19 @@ int main()
 
   sf::CircleShape circle(40);
   circle.setFillColor(sf::Color::Red);
-  circle.setPosition(1,1);
+  circle.setPosition(100,100);
+
+  sf::RectangleShape hero(sf::Vector2f(20,20));
+  hero.setFillColor(sf::Color::Yellow);
+  hero.setPosition(300,500);
+
+
 
   int circleDirection = 0;
+
+  int heroSpeed = 1;
+
+  int points =  10;
 
 //Game Loop
   while(window.isOpen())
@@ -25,7 +36,40 @@ int main()
       {
         window.close();
       }
+
     }
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && hero.getPosition().y > 0)
+    {
+      hero.move(0,-heroSpeed);
+    }
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && hero.getPosition().y < HIGH-hero.getSize().y)
+    {
+      hero.move(0,heroSpeed);
+    }
+
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && hero.getPosition().x < WIDE-hero.getSize().x)
+    {
+      hero.move(heroSpeed,0);
+    }
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && hero.getPosition().x > 0)
+      {
+        hero.move(-heroSpeed,0);
+      }
+
+      if(hero.getGlobalBounds().intersects(circle.getGlobalBounds()))
+      {
+        std::cout << "Touching!" << std::endl;
+      }
+      else
+      {
+        std::cout << "You are not touching" << std::endl;
+      }
+
+
+
+
     window.clear(sf::Color::Green);
     //Update stuff - This is Aaron
     if(circleDirection == 0)
@@ -54,6 +98,7 @@ int main()
 
 
     //Render - this is MEEEEE
+    window.draw(hero);
     window.draw(circle);
     window.display();
   }
